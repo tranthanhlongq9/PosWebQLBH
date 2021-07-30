69daa3594232bd589fe5f4b58f0c91c95d176448
Thanh Long <tranthanhlong.q9@gmail.com>
2021-07-30 16:34:43 +0700

    Phân trang cho User

--- PosWebQLBH.Application/Catalog/Products/ProductService.cs
+++ PosWebQLBH.Application/Catalog/Products/ProductService.cs
@@ -133,7 +133,9 @@ namespace PosWebQLBH.Application.Catalog.Products
             //4. Select and projection
             var pagedResult = new PagedResult<ProductViewModel>()
             {
-                TotalRecord = totalRow,
+                TotalRecords = totalRow,
+                PageIndex = request.PageIndex,
+                PageSize = request.PageSize,
                 Items = data
             };
             return pagedResult;
@@ -305,7 +307,9 @@ namespace PosWebQLBH.Application.Catalog.Products
             //4. Select and projection
             var pagedResult = new PagedResult<ProductViewModel>()
             {
-                TotalRecord = totalRow,
+                TotalRecords = totalRow,
+                PageIndex = request.PageIndex,
+                PageSize = request.PageSize,
                 Items = data
             };
             return pagedResult;
--- PosWebQLBH.ViewModels/System/Users/UserVm.cs
+++ PosWebQLBH.ViewModels/System/Users/UserVm.cs
@@ -11,18 +11,24 @@ namespace PosWebQLBH.ViewModels.System.Users
     {
         public Guid Id { get; set; }
 
+        [Display(Name = "Tên:")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Họ:")]
         public string LastName { get; set; }
 
+        [Display(Name = "Ngày sinh:")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd / MM / yyyy}")] // ( ,ApplyFormatInEditMode = true)
         public DateTime Birthday { get; set; }
 
+        [Display(Name = "Số điện thoại:")]
         public string PhoneNumber { get; set; }
 
+        [Display(Name = "Tài khoản ( Mã nhân viên ):")]
         public string UserName { get; set; }
 
+        [Display(Name = "Email:")]
         public string Email { get; set; }
     }
 }
