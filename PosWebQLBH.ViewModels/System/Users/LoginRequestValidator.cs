4c231f24e8eef49a1a7c44b39ce3d41cd58a5dc6
Thanh Long <tranthanhlong.q9@gmail.com>
2021-08-01 17:31:19 +0700

    fix login

--- PosWebQLBH.ViewModels/System/Users/LoginRequestValidator.cs
+++ PosWebQLBH.ViewModels/System/Users/LoginRequestValidator.cs
@@ -11,9 +11,18 @@ namespace PosWebQLBH.ViewModels.System.Users
     {
         public LoginRequestValidator()
         {
-            RuleFor(x => x.UserName).NotEmpty().WithMessage("Tài khoản không được để trống");
+            //RuleFor(x => x.UserName).NotEmpty().WithMessage("Tài khoản không được để trống");
+            //RuleFor(x => x.Password).NotEmpty().WithMessage("Mật khẩu không được để trống")
+            //    .MinimumLength(6).WithMessage("Mật khẩu phải có ít nhất 6 ký tự !");
+
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Tài khoản không được để trống")
+                .Matches(@"^[a-zA-z0-9@.]*$")
+                .WithMessage("Tài khoản không được sử dụng ký tự đặc biệt, khoảng trắng");
             RuleFor(x => x.Password).NotEmpty().WithMessage("Mật khẩu không được để trống")
-                .MinimumLength(6).WithMessage("Mật khẩu phải có ít nhất 6 ký tự !");
+                .MinimumLength(6).WithMessage("Mật khẩu phải có ít nhất 6 ký tự !")
+                .MaximumLength(30).WithMessage("Mật khẩu không vượt quá 30 ký tự !")
+                .Matches(@"^[a-zA-z0-9@]*$")
+                .WithMessage("Mật khẩu không được sử dụng ký tự đặc biệt, khoảng trắng");
         }
     }
 }
